5741e17caed38e68cc09acfc0e22042d0b527d61
Fede Pujol <federicodpujol@gmail.com>
2018-05-30 12:36:01 -0300

    Solucion Data
    Resolucion con Datatypes.

--- series.hs
+++ series.hs
@@ -4,69 +4,66 @@ type Nombre = String
 type Temporada = Int
 type Anio = Int
 type Cadena = String
-type Serie = (Nombre, Temporada, Anio, Cadena)
-type Actor = (Nombre, [Nombre])
 
-series = [("los soprano", 6, 1999, "HBO"), 
-          ("lost", 6, 2004, "ABC"), 
-          ("4400", 4, 2004, "CBS"),  
-          ("United States of Tara", 3, 2009, "Dreamworks"), 
-          ("V", 3, 2009, "Warner Bross"), 
-          ("dr house", 7, 2004, "Universal")]   
+data Serie = Serie {
+    nombre :: Nombre,
+    temporada :: Temporada,
+    anio :: Anio,
+    cadena :: Cadena
+} deriving(Show, Eq)
 
-actores = [("Ken Leung", ["lost", "los soprano"]), 
-    ("Joel Gretsch", ["4400", "V", "United States of Tara"]), 
-    ("James Gandolfini", ["los soprano"]), 
-    ("Elizabeth Mitchell", ["dr house", "V", "lost"])]  
+data Actor = Actor {
+    nombreActor :: Nombre,
+    seriesActor :: [Serie]
+}
 
--- Primitivas
-serie :: Serie -> Nombre
-serie (s, _, _, _) = s
+losSoprano = Serie "los soprano" 6 1999 "HBO"
+lost = Serie "lost" 4 2004 "ABC"
+the4400 = Serie "4400" 4 2004 "CBS"
+unitedStatesOfTara = Serie "United States of Tara" 3 2009 "Dreamworks"
+v = Serie "V" 3 2009 "Warner Bross"
+drHouse = Serie "dr house" 7 2004 "Universal"
 
-temporadas :: Serie -> Temporada
-temporadas (_, t, _, _) = t
+series :: [Serie]
+series = [losSoprano, lost, the4400, unitedStatesOfTara, v, drHouse]   
 
-anioComienzo :: Serie -> Anio
-anioComienzo (_, _, a, _) = a
+kenLeung = Actor "Ken Leung" [lost, losSoprano]
+joelGretsch = Actor "Joel Gretsch" [the4400, v, losSoprano]
+jamesGandolfini = Actor "James Gandolfini" [losSoprano]
+elizabethMitchell = Actor "Elizabeth Mitchell" [drHouse, v, lost]
 
-cadenaTV :: Serie -> Cadena
-cadenaTV (_, _, _, c) = c
-
-nombreActor :: Actor -> Nombre
-nombreActor = fst
-
-seriesDeActor :: Actor -> [Nombre]
-seriesDeActor = snd
+actores :: [Actor]
+actores = [kenLeung, joelGretsch, jamesGandolfini, elizabethMitchell]  
 
 findCri criterio = head . filter criterio 
 
 -- 1)
-datosDe :: Nombre -> Serie
-datosDe nombreSerie = findCri (nombresIguales nombreSerie) series
+datosDe :: Serie -> Serie
+datosDe unaSerie = findCri (nombresIguales $ nombre unaSerie) series
 
 nombresIguales :: Nombre -> Serie -> Bool
-nombresIguales unNombre unaSerie = (==) unNombre (serie unaSerie)
+nombresIguales unNombre unaSerie = (==) unNombre (nombre unaSerie)
 
 -- 2)
-listaDeActoresDe :: Nombre -> [Nombre]
-listaDeActoresDe nombreSerie = map nombreActor (filtroNombres nombreSerie actores)
+listaDeActoresDe :: Serie -> [Nombre]
+listaDeActoresDe serie = map nombreActor (filtroNombres serie actores)
 
-filtroNombres :: Nombre -> [Actor] -> [Actor]
-filtroNombres nombreSerie listaActores = filter (criterioActor nombreSerie) listaActores
+filtroNombres :: Serie -> [Actor] -> [Actor]
+filtroNombres serie listaActores = filter (criterioActor serie) listaActores
 
-criterioActor :: Nombre -> Actor -> Bool
-criterioActor nombreSerie actor = elem nombreSerie (seriesDeActor actor)
+criterioActor :: Serie -> Actor -> Bool
+criterioActor serie actor = elem serie (seriesActor actor)
 
 -- 3)
-quienesActuaronEn :: Nombre -> Nombre -> [Nombre]
+quienesActuaronEn :: Serie -> Serie -> [Nombre]
 quienesActuaronEn serie1 serie2 = findCri criterioSeries (listaApariciones serie1 serie2)
 
-listaApariciones :: Nombre -> Nombre -> [[Nombre]]
+listaApariciones :: Serie -> Serie -> [[Nombre]]
 listaApariciones serie1 serie2 = map (aparicionesSerie serie1 serie2) actores
 
-aparicionesSerie :: Nombre -> Nombre -> Actor -> [Nombre]
-aparicionesSerie nombreSerie1 nombreSerie2 actor  
-  | criterioActor nombreSerie1 actor && criterioActor nombreSerie2 actor = [nombreActor actor]
+aparicionesSerie :: Serie -> Serie -> Actor -> [Nombre]
+aparicionesSerie serie1 serie2 actor  
+  | criterioActor serie1 actor && criterioActor serie2 actor = [nombreActor actor]
   | otherwise = []
 
 criterioSeries :: [a] -> Bool
@@ -74,46 +71,46 @@ criterioSeries lista = (not . null) lista
 
 -- 4)
 -- a
-anioDeComienzoDe :: Nombre -> Anio
-anioDeComienzoDe = anioComienzo . datosDe
+anioDeComienzoDe :: Serie -> Anio
+anioDeComienzoDe = anio . datosDe
 
 -- b
-seriesOrdenadas :: [Nombre] -> Bool
+seriesOrdenadas :: [Serie] -> Bool
 seriesOrdenadas listaSeries = (==) listaSeries (ordenarListaPorAnio listaSeries)
 
-ordenarListaPorAnio :: [Nombre] -> [Nombre]
+ordenarListaPorAnio :: [Serie] -> [Serie]
 ordenarListaPorAnio listaSeries = 
-  sortBy (\ a b -> compare (anioDeComienzoDe a) (anioDeComienzoDe b)) listaSeries
+  sortBy (\ a b -> compare (anio a) (anio b)) listaSeries
 
 -- 5)
 queSeriesCumplen :: (Serie -> Bool) -> [Serie] -> [Nombre]
-queSeriesCumplen unCriterio = map serie . filter unCriterio
+queSeriesCumplen unCriterio = map nombre . filter unCriterio
 
 -- a
 mayorA3Temporadas :: [Serie] -> [Nombre]
 mayorA3Temporadas = queSeriesCumplen criterioTemporada
 
 criterioTemporada :: Serie -> Bool
-criterioTemporada = (>3) . temporadas
+criterioTemporada = (>3) . temporada
 
 -- b
 masDe4Actores :: [Serie] -> [Nombre]
 masDe4Actores = queSeriesCumplen criterio4Actores
 
 criterio4Actores :: Serie -> Bool
-criterio4Actores = (>4) . length . listaDeActoresDe . serie
+criterio4Actores = (>4) . length . listaDeActoresDe
 
 -- c
 masDe5LetrasTitulo :: [Serie] -> [Nombre]
 masDe5LetrasTitulo = queSeriesCumplen criterio5Letras
 
 criterio5Letras :: Serie -> Bool
-criterio5Letras = (<5) . length . serie
+criterio5Letras = (<5) . length . nombre
 
 -- 6)
 -- a
 promedioGeneral :: Int
-promedioGeneral = div (totalSegun temporadas series) (cantidadSegun temporadas series)
+promedioGeneral = div (totalSegun temporada series) (cantidadSegun temporada series)
   
 totalSegun :: (Serie -> Int) -> [Serie] -> Int
 totalSegun criterio = sum . aplicarCriterio criterio
@@ -130,7 +127,7 @@ promedio criterio =
   div (totalSegun criterio series) (cantidadSegun criterio series)
 
 anioFin :: Serie -> Int
-anioFin unaSerie = temporadas unaSerie + anioComienzo unaSerie
+anioFin unaSerie = temporada unaSerie + anio unaSerie
 
 -- 7)
 funcionHeavy :: (Ord a) => (c -> b) -> (c -> b) -> [c] -> a -> a -> [b]
